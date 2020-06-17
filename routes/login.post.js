f2b05a45d1d113bdcf4af54c1ec968ef86b2f1eb
ryanmurakami <ryanharrisonlewis@gmail.com>
2020-06-17 10:54:15 -0600

    upgrade hapi

--- routes/login.post.js
+++ routes/login.post.js
@@ -9,10 +9,10 @@ module.exports = handlers => ({
       mode: 'try'
     },
     validate: {
-      payload: {
+      payload: Joi.object({
         username: Joi.string().alphanum().min(3).max(30).required(),
         password: Joi.string().min(3).max(30).required()
-      }
+      })
     }
   }
 })
