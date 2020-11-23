e9dd5c5a1e8b6df837b4f563b6cd725d4769cf59
ryanmurakami <ryanharrisonlewis@gmail.com>
2020-11-23 14:02:46 -0700

    rds stuff

---
+++ data/pizzaStore.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize')
+
+const database = 'pizza_luvrs'
+const host = '<enter_host_here>'
+const username = 'postgres'
+const password = 'password'
+
+const pgClient = new Sequelize(
+  database,
+  username,
+  password,
+  {
+    host: host,
+    dialect: 'postgres'
+  }
+)
+
+const Pizza = pgClient.define('pizza', {
+  id: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  toppings: {
+    type: Sequelize.STRING
+  },
+  img: {
+    type: Sequelize.STRING
+  },
+  username: {
+    type: Sequelize.STRING
+  },
+  created: {
+    type: Sequelize.BIGINT
+  }
+})
+
+Pizza.sync({ force: true }).then(() => {
+  console.log('postgres connection ready')
+})
+
+module.exports = Pizza
